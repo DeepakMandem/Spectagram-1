bef30329b26788c1d60cfdf1d44dc9ce9783ac49
DeepakMandem <72325190+DeepakMandem@users.noreply.github.com>
2021-07-27 13:48:23 -0500

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class Feed extends React.Component{
+
+    render(){
+        return(
+            <View>
+                <Text> Feed Screen </Text>
+            </View>
+        )
+    }
+}
